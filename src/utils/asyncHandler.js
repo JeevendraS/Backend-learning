15538b64109ab753e9465b0170b54f3f4f8c09e7
Jeevendra <jeevendrasingh1999@gmail.com>
2024-03-26 09:20:54 +0530

    utilties added

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,11 +1,10 @@
 const asyncHandler = (requestHandler) => {
     (req, res, next)=> {
-        Promise.resolve(requestHandler(req, res, next)).catch((error)=> next(error))
+        Promise.resolve(requestHandler(req, res, next)).catch((err)=> next(err))
     }
 }
 
 export {asyncHandler}
-
 
 
 
@@ -18,9 +17,9 @@ const asyncHandler = (func) => async (req, res, next) => {
     try {
         await func(req, res, next)
     } catch (error) {
-        res.status(error.code || 500).json({
+        res.status(err.code || 500).json({
             success: false,
-            message: error.message
+            message: err.message
         }) 
     }
 } 
