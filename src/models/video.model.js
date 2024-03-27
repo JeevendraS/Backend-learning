0c46dd70ea66cde6f7e3a43a05a616ee735c8e86
Jeevendra <jeevendrasingh1999@gmail.com>
2024-03-27 09:37:33 +0530

    configured cloudinary and multer

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -39,6 +39,6 @@ const videoSchema = new Schema({
 {timestamps: true})
 
 
-videoSchema.plugin(mongooseAggregatePaginate)
+videoSchema.plugin(mongooseAggregatePaginate) 
 
 export const Video = mongoose.model('Video', videoSchema)
