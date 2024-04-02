552e82c99b1ae301d7404081b85aa9e4795252a1
Jeevendra <jeevendrasingh1999@gmail.com>
2024-04-02 15:40:23 +0530

    update

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,27 +1,33 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser, refreshAccessToken } from "../controllers/user.controllers.js"
-import { upload } from "../middlewares/multer.middleware.js"
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+  refreshAccessToken,
+} from "../controllers/user.controllers.js";
+import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
-const router = Router()
+const router = Router();
 
-router.route("/register").post(upload.fields([
+router.route("/register").post(
+  upload.fields([
     {
-        name: "avatar",
-        maxCount: 1
+      name: "avatar",
+      maxCount: 1,
     },
     {
-        name: "coverImage",
-        maxCount: 1
-    }
-]) , registerUser)
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 
-router.route("/login").post(loginUser)
+router.route("/login").post(loginUser);
 
 //secured routes
-router.route("/logout").post(verifyJWT, logoutUser)
-router.route("/refresh-token").post(refreshAccessToken)
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
 
-
-
-export default router
+export default router;
