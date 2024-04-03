925b50dc4d91826581f3df6f014ce17799a3b349
Jeevendra <jeevendrasingh1999@gmail.com>
2024-04-03 21:11:52 +0530

    indentation

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -31,7 +31,7 @@ const userSchema = new Schema({
     coverImage: {
         type: String, // cloudinary url
     },
-    watchhistory: [
+    watchHistory: [
         {
             type: Schema.Types.ObjectId,
             ref: "Video"
