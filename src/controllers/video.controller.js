853060c06b148280882ca1bc4add126559a7e5ea
Jeevendra <jeevendrasingh1999@gmail.com>
2024-04-10 07:25:19 +0530

    add publishvideo controller

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -4,17 +4,37 @@ import {User} from "../models/user.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
-import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import {uploadOnCloudinary} from "../utils/fileUpload.js"
 
 
 const getAllVideos = asyncHandler(async (req, res) => {
     const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query 
     //TODO: get all videos based on query, sort, pagination
+
+    const videos = await Video
+
+    console.log("videos: ", videos)
+    
+    
+    return res
+    .status(200) 
+    .json(new ApiResponse(200, videos, "Account details updated successfully"))
 })
 
 const publishAVideo = asyncHandler(async (req, res) => {
     const { title, description} = req.body
     // TODO: get video, upload to cloudinary, create video
+
+    if([title, description].some((field)=> field.trim() === "")){
+        throw new ApiError(400, "All field are required")
+    }
+
+    console.log("body", req.body, "files: - ", req.files)
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, req.body, "video publishing"))
+
 })
 
 const getVideoById = asyncHandler(async (req, res) => {
