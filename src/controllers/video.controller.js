9945ab6b596dc656dd4710170cfda1691ed98b5b
Jeevendra <jeevendrasingh1999@gmail.com>
2024-04-11 16:56:20 +0530

    add getVideoById controller

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,67 +1,163 @@
-import mongoose, {isValidObjectId} from "mongoose"
-import {Video} from "../models/video.model.js"
-import {User} from "../models/user.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
-import {uploadOnCloudinary} from "../utils/fileUpload.js"
-
+import mongoose, { isValidObjectId } from "mongoose";
+import { Video } from "../models/video.model.js";
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { uploadOnCloudinary } from "../utils/fileUpload.js";
 
 const getAllVideos = asyncHandler(async (req, res) => {
-    const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query 
-    //TODO: get all videos based on query, sort, pagination
+  const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query;
+  //TODO: get all videos based on query, sort, pagination
 
-    const videos = await Video
+  const videos = await Video;
 
-    console.log("videos: ", videos)
-    
-    
-    return res
-    .status(200) 
-    .json(new ApiResponse(200, videos, "Account details updated successfully"))
-})
+  console.log("videos: ", videos);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, videos, "Account details updated successfully"));
+});
 
 const publishAVideo = asyncHandler(async (req, res) => {
-    const { title, description} = req.body
-    // TODO: get video, upload to cloudinary, create video
+  const { title, description } = req.body;
+  const userId = req.user?.id;
+  // TODO: get video, upload to cloudinary, create video
 
-    if([title, description].some((field)=> field.trim() === "")){
-        throw new ApiError(400, "All field are required")
-    }
+  if ([title, description].some((field) => field.trim() === "")) {
+    throw new ApiError(400, "All field are required");
+  }
+  const videoLocalPath = req.files?.videoFile[0]?.path;
+  const thumbnailLocalPath = req.files?.thumbnail[0]?.path;
 
-    console.log("body", req.body, "files: - ", req.files)
+  if (!videoLocalPath || !thumbnailLocalPath) {
+    throw new ApiError(400, "Video and thumbnail required");
+  }
 
-    return res
+  const video = await uploadOnCloudinary(videoLocalPath);
+  const thumbnail = await uploadOnCloudinary(thumbnailLocalPath);
+
+  if (!video || !thumbnail) {
+    throw new ApiError(400, "Cloudinary, Video and thumbnail are required");
+  }
+
+  const createdVideo = await Video.create({
+    videoFile: video.url,
+    thumbnail: thumbnail.url,
+    title,
+    description,
+    duration: video.duration,
+    owner: userId,
+  });
+
+  const finalVideo = await Video.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(createdVideo._id),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "owner",
+        pipeline: [
+          {
+            $project: {
+              fullName: 1,
+              userName: 1,
+              email: 1,
+              avatar: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        owner: {
+          $first: "$owner",
+        },
+      },
+    },
+  ]);
+
+  return res
     .status(200)
-    .json(new ApiResponse(200, req.body, "video publishing"))
-
-})
+    .json(new ApiResponse(200, finalVideo[0], "video published successfully"));
+});
 
 const getVideoById = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
-    //TODO: get video by id
-})
+  const { videoId } = req.params;
+  //TODO: get video by id
+
+  if(!videoId){
+    throw new ApiError(400, "Video id is required")
+  }
+
+  const video = await Video.aggregate([
+    {
+        $match: {
+            _id: new mongoose.Types.ObjectId(videoId) 
+        }
+    },
+    {
+        $lookup: {
+            from: "users",
+            localField: "owner",
+            foreignField: "_id",
+            as: "owner",
+            pipeline: [
+                {
+                    $project: {
+                        fullName: 1,
+                        userName: 1,
+                        email: 1,
+                        avatar: 1,
+                      },
+                }
+            ] 
+        }
+    },
+    {
+        $addFields: {
+            owner: {
+                $first: "$owner"
+            }
+        }
+    }
+  ])
+
+  console.log(video)
+
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(200, video[0], "Video fetched successfully")
+  )
+
+});
 
 const updateVideo = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
-    //TODO: update video details like title, description, thumbnail
-
-})
+  const { videoId } = req.params;
+  //TODO: update video details like title, description, thumbnail
+});
 
 const deleteVideo = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
-    //TODO: delete video
-})
+  const { videoId } = req.params;
+  //TODO: delete video
+});
 
 const togglePublishStatus = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
-})
+  const { videoId } = req.params;
+});
 
 export {
-    getAllVideos,
-    publishAVideo,
-    getVideoById,
-    updateVideo,
-    deleteVideo,
-    togglePublishStatus
-}
+  getAllVideos,
+  publishAVideo,
+  getVideoById,
+  updateVideo,
+  deleteVideo,
+  togglePublishStatus,
+};
