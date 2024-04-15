2147e8c563337ced5be6778085a8073f7d5e8462
Jeevendra <jeevendrasingh1999@gmail.com>
2024-04-15 20:40:14 +0530

    added tweet controller

---
+++ src/controllers/like.controller.js
@@ -0,0 +1,43 @@
+import mongoose, {isValidObjectId} from "mongoose"
+import {Like} from "../models/like.model.js"
+import {ApiError} from "../utils/ApiError.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+import {asyncHandler} from "../utils/asyncHandler.js"
+
+const toggleVideoLike = asyncHandler(async (req, res) => {
+    const {videoId} = req.params
+    const userId = req.user?._id
+    //TODO: toggle like on video
+
+    const likedVideo = await Like.find({video: videoId}) 
+    console.log(userId, likedVideo);
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, likedVideo, "Video like toggled successfully")
+    )
+})
+ 
+const toggleCommentLike = asyncHandler(async (req, res) => {
+    const {commentId} = req.params
+    //TODO: toggle like on comment 
+
+})
+
+const toggleTweetLike = asyncHandler(async (req, res) => {
+    const {tweetId} = req.params
+    //TODO: toggle like on tweet
+}
+)
+
+const getLikedVideos = asyncHandler(async (req, res) => {
+    //TODO: get all liked videos
+})
+
+export {
+    toggleCommentLike,
+    toggleTweetLike,
+    toggleVideoLike,
+    getLikedVideos
+}
