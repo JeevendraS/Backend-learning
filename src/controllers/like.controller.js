8b73d93eb05889e12bbc992ab7c21c2950d1538e
Jeevendra <jeevendrasingh1999@gmail.com>
2024-05-02 14:18:20 +0530

    update

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -15,42 +15,35 @@ const toggleVideoLike = asyncHandler(async (req, res) => {
       throw new ApiError(400, "VideoId is not valid")
     }
 
-    const likeDocument = await Like.findOne({likedBy: userId})
+    const likedVideo = await Like.findOne({
+      video: videoId,
+      likedBy: userId
+    })
 
-    try {
-      if(!likeDocument){
-        const likedVideo = await Like.create({
-          video: videoId,
-          likedBy: userId
-        })
-  
-        if(!likedVideo){
-          throw new ApiError(400, "Something went wrong or server error")
-        }
-  
-        return res
-        .status(200)
-        .json(
-          new ApiResponse(200, likedVideo, "Video liked successfully")
-        )
-      }else{
-        if(!likeDocument.video){
-          likeDocument.video = videoId
-        }else{
-        likeDocument.video = null
-        }
-        await likeDocument.save()
-        
-        return res
-        .status(200)
-        .json(
-          new ApiResponse(200, likeDocument, "Video liked toggled successfully")
-        )
+    if(!likedVideo){
+      const likeVideo = await Like.create({
+        video: videoId,
+        likedBy: userId
+      })
+
+      if(!likeVideo){
+        throw new ApiError(400, "Something went wrong")
       }
-    } catch (error) {
-      console.log(error);
-      throw new ApiError(400, "something went wront")
+
+      return res
+      .status(200)
+      .json(
+        new ApiResponse(200, {videoLike: true}, "video like successfully")
+      )
     }
+
+    const deleteLikedVideo = await Like.findByIdAndDelete(likedVideo._id)
+
+    return res
+    .status(200)
+    .json( 
+      new ApiResponse(200, {videoLike: false}, "video dislike successfully")
+    )
 })
  
 const toggleCommentLike = asyncHandler(async (req, res) => {
@@ -60,46 +53,38 @@ const toggleCommentLike = asyncHandler(async (req, res) => {
     const isCommentValid = isValidObjectId(commentId)
 
     if(!isCommentValid){
-      throw new ApiError(400, "Comment is not valid")
+      throw new ApiError(400, "CommentId is not valid")
     }
 
-    const likeDocument = await Like.findOne({likedBy: userId})
+    const likedComment = await Like.findOne({
+      comment: commentId,
+      likedBy: userId
+    })
 
-    try {
-      if(!likeDocument){
-        const likedComment = await Like.create({
-          comment: commentId,
-          likedBy: userId
-        })
+    if(!likedComment){
+      const likeComment = await Like.create({
+        comment: commentId,
+        likedBy: userId
+      })
 
-        if(!likedComment){
-          throw new ApiError(400, "something went wrong while creating like document")
-        }
-
-        return res
-        .status(200)
-        .json(
-          new ApiResponse(200, likedComment, "Comment liked successfully")
-        )
-      }else{
-        if(!likeDocument.comment){
-          likeDocument.comment = commentId
-        }else{
-          likeDocument.comment = null
-        }
-
-        await likeDocument.save()
-
-        return res
-        .status(200)
-        .json(
-          new ApiResponse(200, likeDocument, "Comment like toggled successfully")
-        )
+      if(!likeComment){
+        throw new ApiError(400, "Something went wrong")
       }
-    } catch (error) {
-      console.log(error)
-      throw new ApiError(400, "Something went wrong")
+
+      return res
+      .status(200)
+      .json(
+        new ApiResponse(200, {commentLike: true}, "comment like successfully")
+      )
     }
+
+    const deleteLikedComment = await Like.findByIdAndDelete(likedComment._id)
+
+    return res
+    .status(200)
+    .json( 
+      new ApiResponse(200, {commentLike: false}, "comment dislike successfully")
+    )
 
 })
 
@@ -110,55 +95,70 @@ const toggleTweetLike = asyncHandler(async (req, res) => {
     const isTweetValid = isValidObjectId(tweetId)
 
     if(!isTweetValid){
-      throw new ApiError(400, "tweet is not valid")
+      throw new ApiError(400, "tweetId is not valid")
     }
 
-    const likeDocument = await Like.findOne({ likedBy: userId})
+    const likedTweet = await Like.findOne({
+      tweet: tweetId,
+      likedBy: userId
+    })
 
-    try {
-      if(!likeDocument){
-        const likedDocument = Like.create({
-          tweet: tweetId,
-          likedBy: userId
-        })
+    if(!likedTweet){
+      const likeTweet = await Like.create({
+        tweet: tweetId,
+        likedBy: userId
+      })
 
-        if(!likedDocument){
-          throw new ApiError(400, "Error while liking the tweet")
-        }
-
-        return res
-        .status(200)
-        .json(
-          new ApiResponse(200, likedDocument, "tweet liked successfully")
-        )
-      }else{
-        if(!likeDocument.tweet){
-          likeDocument.tweet = tweetId
-        }else{
-          likeDocument.tweet = null
-        }
-
-        await likeDocument.save()
-
-        return res
-        .status(200)
-        .json(
-          new ApiResponse(200, likeDocument, "tweet like toggled successfully")
-        )
+      if(!likeTweet){
+        throw new ApiError(400, "Something went wrong")
       }
-    } catch (error) {
-      console.log(error); 
 
-      throw new ApiError(400, "something went wrong")
+      return res
+      .status(200)
+      .json(
+        new ApiResponse(200, {tweetLike: true}, "tweet like successfully")
+      )
     }
 
+    const deleteLikedTweet = await Like.findByIdAndDelete(likedTweet._id)
 
+    return res
+    .status(200)
+    .json( 
+      new ApiResponse(200, {tweetLike: false}, "tweet dislike successfully")
+    )
   }
 )
 
 const getLikedVideos = asyncHandler(async (req, res) => {
-    const userId = req.user?._id                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
-                                           
+    const userId = req.user?._id     
+
+    const allVideos = await Like.aggregate([
+      {
+        $match: {
+          likedBy: userId,
+          video: {"$exists": true, "$ne": null , "$ne": ""}
+        }
+      },
+      {
+        $lookup: {
+          from: "videos",
+          localField: "video",
+          foreignField: "_id",
+          as: "video",
+        },
+      }
+    ])                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+    
+    if(!allVideos){
+      throw new ApiError(400, "Something went wrong while fetching all liked videos")
+    }
+    
+    return res
+    .status(200)
+    .json(
+      new ApiResponse(200, allVideos, "All liked video fetched succussfully")
+    )
 
 })
 
